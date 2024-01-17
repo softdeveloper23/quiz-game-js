b6585f03118bb37fe71c87991693a9749f6fd53f
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-17 22:56:46 +0100

    Added code

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const prompt = require('prompt-sync')();
 console.log("Welcome to the Computer Hardware Quiz!");
 
 let correctAnswers = 0;
+const totalQuestions = 3;
 
 const answer1 = prompt("What is the brain of the computer? ");
 const correctAnswer1 = "CPU"; // Answer needs to be in all caps
@@ -34,4 +35,7 @@ if (answer3.toUpperCase() === correctAnswer3) {
   console.log("Incorrect!");
 }
 
+const percent = Math.round((correctAnswers/totalQuestions) * 100);
+
 console.log("You got " + correctAnswers + " out of 3 questions correct!");
+console.log("Your score is", percent, "% correct!");
